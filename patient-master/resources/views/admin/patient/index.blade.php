5aaf873c70c263da4d0cab5235aa6caf024c24ce
Senthan <senthaneng@gmail.com>
2018-01-21 16:01:00 +0800

    comment it dbclick on patient index

--- patient-master/resources/views/admin/patient/index.blade.php
+++ patient-master/resources/views/admin/patient/index.blade.php
@@ -113,7 +113,7 @@
 
             gridOptions.showGridFooter = true;
 
-            gridOptions.rowTemplate = "<div ng-dblclick=\"grid.appScope.rowDblClick(row)\" ng-repeat=\"(colRenderIndex, col) in colContainer.renderedColumns track by col.colDef.name\" class=\"ui-grid-cell\" ng-class=\"{ 'ui-grid-row-header-cell': col.isRowHeader }\" ui-grid-cell></div>"
+//            gridOptions.rowTemplate = "<div ng-dblclick=\"grid.appScope.rowDblClick(row)\" ng-repeat=\"(colRenderIndex, col) in colContainer.renderedColumns track by col.colDef.name\" class=\"ui-grid-cell\" ng-class=\"{ 'ui-grid-row-header-cell': col.isRowHeader }\" ui-grid-cell></div>"
 
 
             gridOptions.exporterOlderExcelCompatibility = true;
